065c1ed28d60b4c6b1666c2a610f4b5ee7d3873b
Antonio Coppola <info@antonio-coppola.com>
2019-12-03 12:18:04 +0100

    code linting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,33 @@
 module.exports = {
   theme: {
     container: {
-      center: true,
+      center: true
     },
     screens: {
       sm: "640px",
       md: "768px",
       lg: "1024px",
-      xl: "1280px",
+      xl: "1280px"
     },
     fontFamily: {
       display: ["Playfair Display", "serif"],
-      body: ["Graphik", "sans-serif"],
+      body: ["Graphik", "sans-serif"]
     },
     borderWidth: {
       default: "1px",
       "0": "0",
       "2": "2px",
-      "4": "4px",
+      "4": "4px"
     },
     colors: {
       black: "#333333",
       white: "#ffffff",
-      gold: "#856f38",
+      gold: "#856f38"
     },
     extend: {
       spacing: {
         "96": "24rem",
-        "128": "32rem",
+        "128": "32rem"
       }
     }
   }
