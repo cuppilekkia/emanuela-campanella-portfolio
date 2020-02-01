8abe8e5a2c9dec92657f95d625c6a035e4db0cf9
Antonio Coppola <info@antonio-coppola.com>
2020-02-01 12:53:43 +0100

    added cloudinary settings

--- src/js/blocks/slider.js
+++ src/js/blocks/slider.js
@@ -2,14 +2,15 @@ import Flickity from "flickity";
 
 const sliders = document.querySelectorAll(".js-flickity");
 if (sliders && sliders.length >= 1) {
-  sliders.forEach((slider) => {
-    new Flickity(slider, {
+  sliders.forEach(slider => {
+    const inst = new Flickity(slider, {
       wrapAround: true,
       autoPlay: true,
       dragThreshold: 20,
       selectedAttraction: 0.01,
       friction: 0.15,
-      lazyLoad: true,
+      lazyLoad: true
     });
+    return inst;
   });
 }
