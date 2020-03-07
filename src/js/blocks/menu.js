c26f2677b685f2c09568c55461ce174410c9c2ea
Antonio Coppola <info@antonio-coppola.com>
2020-03-08 00:05:43 +0100

    some menu work

--- src/js/blocks/menu.js
+++ src/js/blocks/menu.js
@@ -1,11 +1,13 @@
 const menu = document.querySelector(".js-menu");
 const toggleBtn = document.querySelector(".js-menu-toggle");
+const toggleBtnIcon = document.querySelector(".js-menu-toggle-icon");
 
 if (menu && window.innerWidth <= 768) {
   menu.classList.add("hidden");
 
   toggleBtn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
+    toggleBtnIcon.classList.toggle("lni-menu");
+    toggleBtnIcon.classList.toggle("lni-close");
   });
 }
-
