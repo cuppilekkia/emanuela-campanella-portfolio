d2e1311ce840b2a6cb858d0fd0be5f9b22b8233d
Antonio Coppola <info@antonio-coppola.com>
2020-03-14 19:08:52 +0100

    portfolio single e album

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import "./css/main.css";
 require("./js/blocks/slider.js");
 require("./js/blocks/masonry");
 require("./js/blocks/menu.js");
+require("./js/blocks/portfolio.js");
 // import "./css/slider.css";
